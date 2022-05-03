86c49d66ef38deaae51face320fc81b4985884f0
Emerson C. Romaneli <emerson.romaneli@gmail.com>
2022-05-03 19:01:01 -0300

    Use atomic counter instead of uint++

--- shared/util/sequence/sequence.go
+++ shared/util/sequence/sequence.go
@@ -1,18 +1,26 @@
 package sequence
 
-import "strconv"
+import (
+	"strconv"
+	"sync/atomic"
+)
 
-type Seq uint
+type Seq struct {
+	uint32
+}
 
-var GlobalSeq Seq = 0
+var GlobalSeq Seq = Seq{}
 
-func (seq *Seq) NextUInt() uint {
-	*seq++
-	return uint(*seq)
+func (this *Seq) NextUint32() uint32 {
+	return atomic.AddUint32(&this.uint32, 1)
+}
+
+func (this *Seq) NextUint() uint {
+	return uint(this.NextUint32())
 }
 
 func (seq *Seq) NextInt() int {
-	return int(seq.NextUInt())
+	return int(seq.NextUint32())
 }
 
 func (seq *Seq) NextString() string {
