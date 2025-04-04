5c2f2ed3c7b4f1345aed54e13c983dbc4830a964
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2025-04-03 21:24:49 -0300

    Add brotli and zstd encoding support

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 )
 
 require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
--- internal/app/client/dashboard/service.go
+++ internal/app/client/dashboard/service.go
@@ -8,11 +8,17 @@ import (
 	"woole/internal/app/client/app"
 	"woole/internal/app/client/recorder"
 
-	"github.com/ecromaneli-golang/console/logger"
+	"github.com/andybalholm/brotli"
+	"github.com/klauspost/compress/zstd"
 )
 
 var records = recorder.GetRecords()
 var config = app.ReadConfig()
+var unsupportedContentEncodings = []string{
+	"", // empty
+	"compress",
+	"identity",
+}
 
 func ListenAndServe() error {
 	return setupServer().ListenAndServe(":" + config.DashboardUrl.Port())
@@ -20,35 +26,64 @@ func ListenAndServe() error {
 
 func decompress(contentEncoding string, data []byte) []byte {
 
-	// "compress" content encoding is not supported yet
-	if data == nil || contentEncoding == "" || contentEncoding == "identity" || contentEncoding == "compress" {
+	if data == nil || contains(unsupportedContentEncodings, contentEncoding) {
 		return data
 	}
 
-	var reader io.ReadCloser
-	var err error
+	if contentEncoding == "gzip" {
+		reader, err := gzip.NewReader(bytes.NewReader(data))
+		panicIfNotNil(err)
+		return readReadCloser(reader)
+	} else if contentEncoding == "br" {
+		return readBrotli(brotli.NewReader(bytes.NewReader(data)))
+	} else if contentEncoding == "deflate" {
+		return readReadCloser(flate.NewReader(bytes.NewReader(data)))
+	} else if contentEncoding == "zstd" {
+		decoder, err := zstd.NewReader(bytes.NewReader(data))
+		panicIfNotNil(err)
+		return readZstd(decoder)
+	}
 
+	panic("Unsupported content encoding: " + contentEncoding)
+}
+
+func readReadCloser(reader io.ReadCloser) []byte {
 	defer func() {
 		if reader != nil {
-			err = reader.Close()
+			err := reader.Close()
 			panicIfNotNil(err)
 		}
 	}()
 
-	if contentEncoding == "gzip" {
-		reader, err = gzip.NewReader(bytes.NewReader(data))
-		panicIfNotNil(err)
-	} else if contentEncoding == "br" {
-		// reader = cbrotli.NewReader(bytes.NewReader(data))
-		logger.GetInstance().Error("brotli not supported yet")
-	} else if contentEncoding == "deflate" {
-		reader = flate.NewReader(bytes.NewReader(data))
-	}
-
-	data, err = io.ReadAll(reader)
+	data, err := io.ReadAll(reader)
 	panicIfNotNil(err)
 
 	return data
+}
+
+func readZstd(reader *zstd.Decoder) []byte {
+	defer reader.Close()
+
+	data, err := io.ReadAll(reader)
+	panicIfNotNil(err)
+
+	return data
+}
+
+func readBrotli(reader *brotli.Reader) []byte {
+	data, err := io.ReadAll(reader)
+	panicIfNotNil(err)
+
+	return data
+}
+
+func contains(slice []string, target string) bool {
+	for _, str := range slice {
+		if str == target {
+			return true
+		}
+	}
+	return false
 }
 
 func panicIfNotNil(err any) {
