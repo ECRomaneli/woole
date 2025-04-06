f6d02f872bbe1c65fd35fee18beee9ed988e6b9d
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2025-04-05 21:47:23 -0300

    Fix generated tunnel URL info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 
 	"woole/internal/app/server/app"
 	"woole/internal/app/server/recorder"
@@ -18,9 +19,31 @@ func main() {
 		fmt.Printf(" HTTPS listening on https://%s:%s\n", config.HostnamePattern, config.HttpsPort)
 	}
 
-	fmt.Printf("Tunnel listening on grpc://%s:%s\n", config.HostnamePattern, config.TunnelPort)
+	localIp, err := getLocalIp()
+	if err != nil {
+		fmt.Printf("Tunnel listening on grpc://<hostname-or-ip>:%s\n", config.TunnelPort)
+	} else {
+		fmt.Printf("Tunnel listening on grpc://%s:%s\n", localIp, config.TunnelPort)
+	}
 	fmt.Println("===============")
 	fmt.Println()
 
 	recorder.Start()
+}
+
+func getLocalIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil { // Verifica se é IPv4
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+
+	return "", fmt.Errorf("failed to retrieve the local ip")
 }
