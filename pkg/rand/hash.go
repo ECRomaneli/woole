14c5e13254e6196fd2fc28b437ed284bcbacac31
Emerson Capuchi Romaneli <emerson.romaneli@gmail.com>
2025-04-10 09:00:25 -0300

    Rename hash parameter

--- pkg/rand/hash.go
+++ pkg/rand/hash.go
@@ -10,29 +10,29 @@ import (
 	"time"
 )
 
-func RandMD5(var1 string) []byte { // 16
-	hash := md5.Sum(generateUniqueByteArr(var1))
+func RandMD5(salt string) []byte { // 16
+	hash := md5.Sum(generateUniqueByteArr(salt))
 	return hash[:]
 }
 
-func RandSha1(var1 string) []byte { // 20
-	hash := sha1.Sum(generateUniqueByteArr(var1))
+func RandSha1(salt string) []byte { // 20
+	hash := sha1.Sum(generateUniqueByteArr(salt))
 	return hash[:]
 }
 
-func RandSha256(var1 string) []byte { // 32
-	hash := sha256.Sum256(generateUniqueByteArr(var1))
+func RandSha256(salt string) []byte { // 32
+	hash := sha256.Sum256(generateUniqueByteArr(salt))
 	return hash[:]
 }
 
-func RandSha512(var1 string) []byte { // 64
-	hash := sha512.Sum512(generateUniqueByteArr(var1))
+func RandSha512(salt string) []byte { // 64
+	hash := sha512.Sum512(generateUniqueByteArr(salt))
 	return hash[:]
 }
 
-func generateUniqueByteArr(var1 string) []byte {
+func generateUniqueByteArr(salt string) []byte {
 	r1 := time.Now().UnixNano()
 	r2 := rand.Int()
 	r3 := rand.Int()
-	return []byte(strconv.FormatInt(r1, 16) + var1 + strconv.Itoa(r2) + strconv.Itoa(r3))
+	return []byte(strconv.FormatInt(r1, 16) + salt + strconv.Itoa(r2) + strconv.Itoa(r3))
 }
